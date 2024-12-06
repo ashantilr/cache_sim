e3db18c0a1e384872d5b7820bff9e189b0909f26
Ashanti <106094234+ashantilr@users.noreply.github.com>
2024-12-06 13:44:48 -0500

    Delete bits.h

--- bits.h
+++
@@ -1,9 +0,0 @@
-#ifndef BITS_H_
-#define BITS_H_
-#include "cache.h"
-
-int get_set(Cache *cache, address_type address);
-int get_line(Cache *cache, address_type address);
-int get_byte(Cache *cache, address_type address);
-
-#endif /* BITS_H_ */
